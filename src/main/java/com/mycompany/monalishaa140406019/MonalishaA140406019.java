68cbd6c0e6a89b7552ce1e32223eb6c748415cda
40406019 <40406019@HY-R103-006.resource.uc>
2025-12-11 11:53:15 +0000

    Working on progress...

---
+++ src/main/java/com/mycompany/monalishaa140406019/MonalishaA140406019.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.monalishaa140406019;
+
+/**
+ *
+ * @author 40406019
+ */
+public class MonalishaA140406019 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
